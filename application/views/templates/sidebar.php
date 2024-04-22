a7402435ce0c8057ba00ca39f0888418491e1f61
Rohman888 <abd.rohman8889@gmail.com>
2024-04-22 23:19:56 +0700

    update

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -4,7 +4,7 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fa-thin fa-certificate"></i>
+                    <i class="fas fa-thin fa-certificate"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">Certificate Generator</div>
             </a>
